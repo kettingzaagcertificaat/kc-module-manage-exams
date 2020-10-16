1cf4caeaeab146caa37ee634d562b93273a62d37
Didier Hartong <info@focuswider.com>
2020-10-16 21:22:06 +0200

    feat(layout): KC layout

---
+++ public/ClientScripts.js
@@ -0,0 +1,24 @@
+function OnClientCommandExecuting(editor, args) {
+	if (typeof jQuery === "undefined" || typeof jQuery.fn.dnnControlPanel === "undefined") return;
+	
+    const commandName = args.get_commandName();
+    if (commandName == "ToggleScreenMode") {
+        if (editor._isFullScreen == true ) {
+            jQuery.fn.dnnControlPanel.show();
+        }
+        else {
+        	jQuery.fn.dnnControlPanel.hide();
+        }
+    }
+}
+
+function OnDnnEditorClientSubmit(editor) {
+    editor.set_mode(1);
+    if (editor.get_mode() != 1) {
+        editor.set_mode(2);
+    }
+}
+
+if (typeof(Sys) != "undefined"){
+    Sys.Application.notifyScriptLoaded() ;
+}
